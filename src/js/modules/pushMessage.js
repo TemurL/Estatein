a4fbb3591d0da50b19e94e0b055ea0cbef06bd72
Temur Lakerbaya <lakerbaya00@mail.ru>
2023-12-12 14:37:47 +0300

    adaptive header

--- src/js/modules/pushMessage.js
+++ src/js/modules/pushMessage.js
@@ -1,6 +1,8 @@
 export const pushMessage = () => {
     const pushElem = document.getElementsByClassName('push-message')[0];
     if (!pushElem) return
+    setTimeout(() => pushElem.setAttribute('open', 'true'), 1000);
+
     const closeBtn = document.getElementsByClassName('push-message__closeBtn')[0];
 
     closeBtn.addEventListener('click', () => !JSON.parse(pushElem.getAttribute('open')) ? pushElem.setAttribute('open', 'true') : pushElem.setAttribute('open', 'false'));
