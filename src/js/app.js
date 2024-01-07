89a8cca25323153c77b90eb690d066fb9ced12d2
Temur Lakerbaya <lakerbaya00@mail.ru>
2024-01-07 22:02:17 +0300

    slider does not work

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,11 @@ import * as TLFunc from './modules/functions.js'
 import { pushMessage } from './modules/pushMessage.js'
 import { ringTextRender } from './modules/ringTextRender.js';
 import { setActiveNavLi } from './modules/setActiveNavLi.js';
+import { uniSlider } from './modules/uniSlider.js';
 
 TLFunc.isWebp()
 
 pushMessage();
 setActiveNavLi();
 ringTextRender();
+uniSlider(3);
