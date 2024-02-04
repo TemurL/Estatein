6a58a5d3b61d0944f1d90cb8c6451198888c5c38
Temur Lakerbaya <lakerbaya00@mail.ru>
2024-02-04 22:34:58 +0300

    better forms on Prop.html

--- src/js/modules/budgetRange.js
+++ src/js/modules/budgetRange.js
@@ -1,17 +1,36 @@
 export const budgetRange = () => {
-    const budgetBlock = document.getElementsByClassName('contact-form-1__budget-block')[0];
+    const budgetBlock = document.querySelector('.contact-form-1__budget-block');
 
     if (!budgetBlock) return
 
     const budget = budgetBlock.querySelector('.contact-form-1__budget');
 
-    budgetBlock.addEventListener('click', (e) => {
-        budget.classList.toggle('show');
-    })
-    budgetBlock.addEventListener('mouseleave', (e) => {
-        budget.classList.remove('show');
-        budgetBlock.querySelector('p').textContent = `
-            Range: ${budget.querySelector('#contact-form-1__budget_from').value} - ${budget.querySelector('#contact-form-1__budget_to').value}
-        `;
-    })
+    const budgetClickHandler = (e) => {
+        e.stopImmediatePropagation()
+    }
+
+    const budgetBlockClickHandler = (e) => {
+        e.stopPropagation()
+        if (budget.classList.contains('show')) {
+            budget.classList.remove('show');
+            budget.removeEventListener('click', budgetClickHandler);
+            e.currentTarget.querySelector('p.label').textContent = `
+                ${Intl.NumberFormat('ru', {
+                    style: 'currency',
+                    currency: 'USD',
+                }).format(budget.querySelector('#contact-form-1__budget_from').value)}
+                  -  
+                ${Intl.NumberFormat('ru', {
+                    style: 'currency',
+                    currency: 'USD',
+                }).format(budget.querySelector('#contact-form-1__budget_to').value)}
+            `;
+        } else {
+            budget.classList.add('show');
+            budget.addEventListener('click', budgetClickHandler);
+        }
+    }
+
+    budgetBlock.addEventListener('click', budgetBlockClickHandler);
+
 };
--- src/js/modules/pricingRange.js
+++ src/js/modules/pricingRange.js
@@ -3,23 +3,35 @@ export const pricingRange = () => {
     if (!selectElement) return;
 
     const rangeBlock = document.getElementsByClassName('search-form__range-block')[0];
+    const rangeBlockClickHandler = (e) => {
+        e.stopImmediatePropagation();
+    }
+
+    const formatOpts = {
+        style: 'currency',
+        currency: 'USD',
+    }
+
+    const selectElementMouseOutHandler = (e) => {
+        rangeBlock.classList.remove('show');
+        selectElement.children[0].textContent = `
+            Up To: ${Intl.NumberFormat('ru', formatOpts).format(rangeBlock.children[2].value)}
+        `
+        rangeBlock.removeEventListener('click', rangeBlockClickHandler);
+        e.currentTarget.removeEventListener('mouseleave', selectElementMouseOutHandler);
+    }
 
     selectElement.addEventListener('click', (e) => {
         rangeBlock.classList.toggle('show');
-    })
-    selectElement.addEventListener('mouseleave', (e) => {
-        rangeBlock.classList.remove('show');
-        selectElement.children[0].textContent = `
-            Up To: $${rangeBlock.children[2].value}
-        `
+        rangeBlock.addEventListener('click', rangeBlockClickHandler);
+        e.currentTarget.addEventListener('mouseleave', selectElementMouseOutHandler);
     })
 
     const rengeInput = document.getElementById('pricing');
 
     const showValueToUser = (e) => {
-        let parag = e.target.parentElement.children[1];
-        parag.textContent = `Selected: $${e.target.value}`;
-        console.log('done');
+        let parag = e.currentTarget.parentElement.children[1];
+        parag.textContent = `Selected: ${Intl.NumberFormat('ru', formatOpts).format(e.currentTarget.value)}`;
     }
 
     rengeInput.addEventListener('change', showValueToUser);
